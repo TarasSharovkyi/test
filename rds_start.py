b70fa4264a7bfa3b1e6254b4ab39583eb61a551b
TarasSharovskyi <taras.sharovskyi@sigma.software>
2022-09-01 13:42:05 +0300

    smthing

--- rds_start.py
+++ rds_start.py
@@ -31,28 +31,24 @@ def start_rds_all():
                 resp2 = client.list_tags_for_resource(ResourceName=arn)
                 # check if the RDS instance is part of the Auto-Shutdown group.
                 if 0 == len(resp2['TagList']):
-                    print('DB Instance {0} is not part of autoshutdown'.format(i['DBInstanceIdentifier']))
+                    print(f'DB Instance {i["DBInstanceIdentifier"]} is not part of autoshutdown')
                 else:
                     for tag in resp2['TagList']:
                         if tag['Key'] == key and tag['Value'] == value:
                             if i['DBInstanceStatus'] == 'available':
-                                print('{0} DB instance is already available'
-                                      .format(i['DBInstanceIdentifier']))
+                                print(f'{i["DBInstanceIdentifier"]} DB instance is already available')
                             elif i['DBInstanceStatus'] == 'stopped':
                                 client.start_db_instance(DBInstanceIdentifier=i['DBInstanceIdentifier'])
-                                print('Started DB Instance {0}'.format(i['DBInstanceIdentifier']))
+                                print(f'Started DB Instance {i["DBInstanceIdentifier"]}')
                             elif i['DBInstanceStatus'] == 'starting':
-                                print('DB Instance {0} is already in starting state'
-                                      .format(i['DBInstanceIdentifier']))
+                                print(f'DB Instance {0} is already in starting state')
                             elif i['DBInstanceStatus'] == 'stopping':
-                                print('DB Instance {0} is in stopping state. Please wait before starting'
-                                      .format(i['DBInstanceIdentifier']))
+                                print(f'DB Instance {i["DBInstanceIdentifier"]} '
+                                      f'is in stopping state. Please wait before starting')
                         elif tag['Key'] != key and tag['Value'] != value:
-                            print('DB instance {0} is not part of autoshutdown'
-                                  .format(i['DBInstanceIdentifier']))
+                            print(f'DB instance {i["DBInstanceIdentifier"]} is not part of autoshutdown')
                         elif len(tag['Key']) == 0 or len(tag['Value']) == 0:
-                            print('DB Instance {0} is not part of autoShutdown'
-                                  .format(i['DBInstanceIdentifier']))
+                            print(f'DB Instance {i["DBInstanceIdentifier"]} is not part of autoshutdown')
             elif i['DBInstanceIdentifier'] in v_read_replica:
                 print(f'DB Instance {i["DBInstanceIdentifier"]} is a Read Replica.')
             else:
@@ -64,24 +60,23 @@ def start_rds_all():
         cluarn = i['DBClusterArn']
         resp2 = client.list_tags_for_resource(ResourceName=cluarn)
         if 0 == len(resp2['TagList']):
-            print('DB Cluster {0} is not part of autoshutdown'.format(i['DBClusterIdentifier']))
+            print(f'DB Cluster {i["DBClusterIdentifier"]} is not part of autoshutdown')
         else:
             for tag in resp2['TagList']:
                 if tag['Key'] == key and tag['Value'] == value:
                     if i['Status'] == 'available':
-                        print('{0} DB Cluster is already available'.format(i['DBClusterIdentifier']))
+                        print(f'{i["DBClusterIdentifier"]} DB Cluster is already available')
                     elif i['Status'] == 'stopped':
                         client.start_db_cluster(DBClusterIdentifier=i['DBClusterIdentifier'])
-                        print('Started Cluster {0}'.format(i['DBClusterIdentifier']))
+                        print(f'Started Cluster {i["DBClusterIdentifier"]}')
                     elif i['Status'] == 'starting':
-                        print('cluster {0} is already in starting state.'.format(i['DBClusterIdentifier']))
+                        print(f'cluster {i["DBClusterIdentifier"]} is already in starting state.')
                     elif i['Status'] == 'stopping':
-                        print('cluster {0} is in stopping state. Please wait before starting'.format(
-                            i['DBClusterIdentifier']))
+                        print(f'cluster {i["DBClusterIdentifier"]} is in stopping state. Please wait before starting')
                 elif tag['Key'] != key and tag['Value'] != value:
-                    print('DB Cluster {0} is not part of autoshutdown'.format(i['DBClusterIdentifier']))
+                    print(f'DB Cluster {i["DBClusterIdentifier"]} is not part of autoshutdown')
                 else:
-                    print('DB Instance {0} is not part of autoShutdown'.format(i['DBClusterIdentifier']))
+                    print(f'DB Instance {i["DBClusterIdentifier"]} is not part of autoShutdown')
 
 
 def lambda_handler(event, context):
