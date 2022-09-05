f5351bac4ec8ef9d95b86b00556ae25e5445bf4b
TarasSharovskyi <taras.sharovskyi@sigma.software>
2022-09-05 20:52:01 +0300

    smthing

--- extract_layer/source_reader.py
+++ extract_layer/source_reader.py
@@ -1,15 +1,16 @@
 """
 SOMETHING
 """
-from bs4 import BeautifulSoup
-import requests
-import re
 import calendar
-import datetime
-from datetime import date
+import re
+import requests
+from bs4 import BeautifulSoup
+# import datetime
+# from datetime import date
 
 
-def process_data_to_tableview(today, my_date, week_num: int, all_engine_types: dict, two_word_car_brands: dict):
+def process_data_to_tableview(today, my_date, week_num: int,
+                              all_engine_types: dict, two_word_car_brands: dict):
     """
     this method extracts data from a source,
     transforms engine types, gearbox types, 2-word brand names etc.
@@ -24,11 +25,15 @@ def process_data_to_tableview(today, my_date, week_num: int, all_engine_types: d
         if len(items) != 0:
             for item in items:
                 car_brand_and_model_list = get_brand_model_from_item(item, two_word_car_brands)
-                mileage = re.findall('[0-9]+', str.lstrip(item.find('li', class_='item-char js-race').text))
+                mileage = re.findall('[0-9]+',
+                                     str.lstrip(item.find('li',
+                                                          class_='item-char js-race').text))
                 year = str.strip(item.find('a', class_='address').text)
-                mileage_loc_engine_gear = re.split('\s{3,}',
-                                                   str.strip(item.find('ul', class_="unstyle characteristic").text))
-                location = str.strip(item.find('li', class_='item-char view-location js-location').text)
+                mileage_loc_engine_gear = re.split(r'\s{3,}',
+                                                   str.strip(item.find('ul',
+                                                                       class_="unstyle characteristic").text))
+                location = str.strip(item.find('li',
+                                               class_='item-char view-location js-location').text)
                 price = str.replace(item.find('span', class_='size15').text, ' ', '').split('$')
                 engine_volume = mileage_loc_engine_gear[2].split(', ')
                 if len(engine_volume) < 2:
@@ -71,7 +76,9 @@ def get_data_from_source(page_number: int):
     Parser method
     """
     r = requests.get(
-        f'https://auto.ria.com/uk/search/?indexName=auto&categories.main.id=1&country.import.usa.not=-1&price.currency=1&top=2&abroad.not=0&custom.not=-1&page={page_number}&size=100'
+        f'https://auto.ria.com/uk/search/?indexName=auto&categories.main.id=1'
+        f'&country.import.usa.not=-1&price.currency=1&top=2'
+        f'&abroad.not=0&custom.not=-1&page={page_number}&size=100'
     )
 
     soup = BeautifulSoup(r.content, 'html.parser')
